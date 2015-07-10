b97073b22c1e21bf080398b7fb4307d50a775eaf
hyunhye <940413_@naver.com>
2015-07-10 19:23:56 +0900

    fixed #3 login

---
+++ BANgTO/gen/com/ohhonghong/bangto/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ohhonghong.bangto;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- BANgTO/src/com/ohhonghong/bangto/MainActivity.java
+++ BANgTO/src/com/ohhonghong/bangto/MainActivity.java
@@ -1,34 +1,117 @@
 package com.ohhonghong.bangto;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener, ConnectionCallbacks, OnConnectionFailedListener {
+
+	private static final String TAG = "ExampleActivity";
+	private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+
+	private ProgressDialog mConnectionProgressDialog;
+	private GoogleApiClient mPlusClient;
+	private ConnectionResult mConnectionResult;
+
+	//Button sign_in_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mPlusClient = new GoogleApiClient.Builder(this).addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+				.addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+		// Progress bar to be displayed if the connection failure is not
+		// resolved.
+		mConnectionProgressDialog = new ProgressDialog(this);
+		mConnectionProgressDialog.setMessage("Signing in...");
+
+		//sign_in_button = (Button) findViewById(R.id.sign_in_button);
+		findViewById(R.id.sign_in_button).setOnClickListener(this);
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onStart() {
+		super.onStart();
+		mPlusClient.connect();
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	protected void onStop() {
+		super.onStop();
+		mPlusClient.disconnect();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// TODO Auto-generated method stub
+		if (mConnectionProgressDialog.isShowing()) {
+			// The user clicked the sign-in button already. Start to resolve
+			// connection errors. Wait until onConnected() to dismiss the
+			// connection dialog.
+			if (result.hasResolution()) {
+				try {
+					result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+				} catch (SendIntentException e) {
+					mPlusClient.connect();
+				}
+			}
 		}
-		return super.onOptionsItemSelected(item);
+		// Save the result and resolve the connection failure upon a user click.
+		mConnectionResult = result;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+		if (requestCode == REQUEST_CODE_RESOLVE_ERR && responseCode == RESULT_OK) {
+			mConnectionResult = null;
+			mPlusClient.connect();
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		// TODO Auto-generated method stub
+		// String accountName = mPlusClient.getAccountName();
+		// Toast.makeText(this, accountName + " is connected.",
+		// Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onConnectionSuspended(int cause) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+
+		if (view.getId() == R.id.sign_in_button && !mPlusClient.isConnected()) {
+			if (mConnectionResult == null) {
+				mConnectionProgressDialog.show();
+			} else {
+				try {
+					mConnectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+				} catch (SendIntentException e) {
+					// Try connecting again.
+					mConnectionResult = null;
+					mPlusClient.connect();
+				}
+			}
+		}
 	}
 }
